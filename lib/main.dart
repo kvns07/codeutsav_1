9cb0b19575c81ab18d9ef64de944c52a3b5f001a
amanjain200 <amanjain189200@gmail.com>
2023-11-02 02:26:20 +0530

    UI changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,87 +1,15 @@
+import 'package:codeutsav_1/home.dart';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import 'display/display.dart';
 import 'repo/post.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final cameras = await availableCameras();
-  final firstCamera = cameras.first;
 
   runApp(
     MaterialApp(
-      home: TakePictureScreen(
-        camera: firstCamera,
-      ),
+      home: Home(),
     ),
   );
-}
-
-class TakePictureScreen extends StatefulWidget {
-  final CameraDescription camera;
-
-  const TakePictureScreen({
-    Key? key,
-    required this.camera,
-  }) : super(key: key);
-
-  @override
-  _TakePictureScreenState createState() => _TakePictureScreenState();
-}
-
-class _TakePictureScreenState extends State<TakePictureScreen> {
-  late CameraController _controller;
-  late Future<void> _initializeControllerFuture;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = CameraController(
-      widget.camera,
-      ResolutionPreset.medium,
-    );
-    _initializeControllerFuture = _controller.initialize();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Take a Picture')),
-      body: FutureBuilder<void>(
-        future: _initializeControllerFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return CameraPreview(_controller);
-          } else {
-            return Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          try {
-            await _initializeControllerFuture;
-            final image = await _controller.takePicture();
-            uploadFile(image.path);
-            // Navigate to a new screen to display the captured image
-            // Navigator.push(
-            //   context,
-            //   MaterialPageRoute(
-            //     builder: (context) => DisplayPictureScreen(imagePath: image.path),
-            //   ),
-            // );
-          } catch (e) {
-            print("Error taking picture: $e");
-          }
-        },
-        child: Icon(Icons.camera),
-      ),
-    );
-  }
-
 }
